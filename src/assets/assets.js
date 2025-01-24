329262722208564cfece40196c234bb89d21ba63
Saurabh negi <negis9705@gmail.com>
2025-01-24 10:33:00 +0530

    add 3 section

---
+++ src/assets/assets.js
@@ -0,0 +1,47 @@
+import HeaderSvg from '../assets/svgjsx/headerSvg.jsx'
+import Creative from '../assets/svgjsx/creative.jsx'
+import brand1 from "../assets/images/brand1.png";
+import brand2 from "../assets/images/brand2.png";
+import brand3 from "../assets/images/brand3.png";
+import brand4 from "../assets/images/brand4.webp";
+import brand5 from "../assets/images/brand5.png";
+import brand6 from "../assets/images/brand6.webp";
+import brand7 from "../assets/images/brand7.webp";
+import brand8 from "../assets/images/brand8.png";
+import brand9 from "../assets/images/brand9.png";
+import technologyIcon1 from "../assets/images/technologyIcon1.png"
+import technologyIcon2 from "../assets/images/technologyIcon2.png"
+import technologyIcon3 from "../assets/images/technologyIcon3.png"
+import technologyIcon4 from "../assets/images/technology4.png"
+import technologyIcon5 from "../assets/images/technologyIcon5.png"
+import technologyIcon6 from "../assets/images/technologyIcon6.png"
+import technologyIcon7 from "../assets/images/technologyIcon7.png"
+import technologyIcon8 from "../assets/images/technologyIcon8.png"
+import technologyIcon9 from "../assets/images/technologyIcon9.png"
+import technologyIcon10 from "../assets/images/technologyIcon10.png"
+
+export const brandImgs = [
+   brand1,
+   brand2,
+   brand3,
+   brand4,
+   brand5,
+   brand6,
+   brand7,
+   brand8,
+   brand9,
+]
+
+
+export const technologyIcons = {
+    technologyIcon1,
+    technologyIcon2,
+    technologyIcon3,
+    technologyIcon4,
+    technologyIcon5,
+    technologyIcon6,
+    technologyIcon7,
+    technologyIcon8,
+    technologyIcon9,
+    technologyIcon10
+}
